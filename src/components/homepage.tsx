8e162640f00619418f40ecbf0aa865c9af7f5ec1
Yassir <atertay@gmail.com>
2025-10-24 21:57:21 +0100

    refactor: new file

---
+++ src/components/homepage.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import About from '@/components/about';
+import Catalogue from '@/components/catalogue';
+import Destinations from '@/components/destinations';
+import Footer from '@/components/footer';
+import Hero from '@/components/hero-section';
+import Testimonials from '@/components/testimonials';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+export default function HomePage() {
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.to('#hero', {
+      rotate: 2,
+      scale: 0.9,
+    });
+
+    ScrollTrigger.create({
+      animation: tl,
+      trigger: '#hero',
+      start: 'top top',
+      end: 'bottom 80%',
+      scrub: 1,
+    });
+
+    const desti = gsap.timeline();
+    desti.to('#desti', {
+      rotate: -2,
+      scale: 0.9,
+    });
+    ScrollTrigger.create({
+      animation: desti,
+      trigger: '#desti',
+      start: 'top top',
+      end: 'bottom 80%',
+      scrub: 1,
+    });
+  });
+
+  return (
+    <div className="font-sans scroll-smooth overflow-x-hidden">
+      <main
+        className="min-h-dvh bg-[linear-gradient(to_bottom,rgba(0,0,0,0.5),rgba(0,0,0,0.8)),url('/anders-jilden-cYrMQA7a3Wc-unsplash.jpg')] bg-no-repeat bg-cover bg-center grid grid-rows-[auto_1fr_auto] sticky top-0"
+        id="hero">
+        <Hero />
+      </main>
+      <main
+        className="sticky top-0 min-h-dvh bg-gradient-to-br from-neutral-950 to-slate-950 text-neutral-100"
+        id="desti">
+        <Destinations />
+      </main>
+      <main className="sticky top-0 min-h-dvh text-neutral-100">
+        <div className="bg-gray-950" id="about">
+          <About />
+        </div>
+        <div className="bg-neutral-950" id="testimonial">
+          <Testimonials />
+        </div>
+        <div
+          className="bg-[linear-gradient(to_bottom,rgba(0,0,0,0.5),rgba(0,0,0,0.8)),url('/dariusz-sankowski-3OiYMgDKJ6k-unsplash.jpg')] bg-no-repeat bg-cover bg-center"
+          id="catalogue">
+          <Catalogue />
+        </div>
+        <div className="bg-neutral-950" id="footer">
+          <Footer />
+        </div>
+      </main>
+    </div>
+  );
+}
